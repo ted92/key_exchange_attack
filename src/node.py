a57dea6729f9745ac07dfd73f33d69e439ea934d
Enrico Tedeschi <enrico.tedeschi@uit.no>
2023-12-12 12:16:33 +0100

    latest updates

--- src/node.py
+++ src/node.py
@@ -12,10 +12,9 @@ import pickle
 import time
 from utils import OK, Verifier, aes_encode, TIME, generate_nonce, verify_nonce
 
-SHARED_KEY = b'TheForceIsString'  # 16bit AES key
+SHARED_KEY = b'TheForceIsStrong'  # 16bit AES key
 
 
-# todo: visualize pretty prints for message exchange
 class Node:
     """
     Node class.
